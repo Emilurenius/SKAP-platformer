d94c0872f9ee22fcfb9ed1425e55f8e50752743e
Minyguy <benjaminleikvold@gmail.com>
2022-01-24 18:23:53 +0100

    stuff

--- skap_plattformer/skap_plattformer/main.py
+++ skap_plattformer/skap_plattformer/main.py
@@ -19,14 +19,14 @@ Example of Pymunk Physics Engine Platformer
 SCREEN_TITLE = "PyMunk Platformer"
 
 # How big are our image tiles?
-SPRITE_IMAGE_SIZE = 128
+GRID_PIXEL_SIZE = 128
 
 # Scale sprites up or down
 SPRITE_SCALING_PLAYER = 0.5
 SPRITE_SCALING_TILES = 0.5
 
 # Scaled sprite size for tiles
-SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
+SPRITE_SIZE = int(GRID_PIXEL_SIZE * SPRITE_SCALING_PLAYER)
 
 # Size of grid to show on screen, in number of tiles
 SCREEN_GRID_WIDTH = 25
@@ -35,6 +35,16 @@ SCREEN_GRID_HEIGHT = 15
 # Size of screen to show, in pixels
 SCREEN_WIDTH = SPRITE_SIZE * SCREEN_GRID_WIDTH
 SCREEN_HEIGHT = SPRITE_SIZE * SCREEN_GRID_HEIGHT
+
+# GUI placements
+TIMER_FROM_RIGHT = 100
+TIMER_FROM_TOP = 20
+SCORE_FROM_LEFT = 20
+SCORE_FROM_TOP = 20
+
+# Constants for color
+WHITE = arcade.color.WHITE
+
 
 # --- Physics forces. Higher number, faster accelerating.
 
@@ -92,7 +102,7 @@ class GameWindow(arcade.Window):
         self.score = 0
 
         # Timer
-        self.total_timer = 0.0
+        self.total_time = 0.0
         self.real_timer_from_right = 60
 
         # Load sounds
@@ -105,10 +115,12 @@ class GameWindow(arcade.Window):
         arcade.set_background_color(arcade.color.AMAZON)
 
         # Sprite lists we need
-        self.player_list: Optional[arcade.SpriteList] = None
-        self.wall_list: Optional[arcade.SpriteList] = None
-        self.bullet_list: Optional[arcade.SpriteList] = None
-        self.item_list: Optional[arcade.SpriteList] = None
+        self.player_list = None
+        self.wall_list = None
+        self.bullet_list = None
+        self.item_list = None
+        self.coin_list = None
+        self.ice_list = None
 
         # Track the current state of what key is pressed
         self.left_pressed: bool = False
@@ -121,31 +133,40 @@ class GameWindow(arcade.Window):
     def setup(self):
         """ Set up everything with the game """
 
-        # Create the sprite lists
-        self.player_list = arcade.SpriteList()
-        self.bullet_list = arcade.SpriteList()
+        # Create Camera
+        self.player_camera = arcade.Camera(self.width, self.height)
+        self.gui_camera = arcade.Camera(self.width, self.height)
 
         # Map name
         map_name = "assets/levels/secretTestLevel.tmx"
 
+        # Create the sprite lists
+        self.player_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+
         # Load in TileMap
-        tile_map = arcade.load_tilemap(path(map_name), SPRITE_SCALING_TILES)
+        self.tile_map = arcade.load_tilemap(path(map_name), SPRITE_SCALING_TILES)
 
-        # Pull the sprite layers out of the tile map
-        self.ground_list = tile_map.sprite_lists["Ground"]
-        self.ice_list = tile_map.sprite_lists['Ice']
-        self.item_list = tile_map.sprite_lists['Item']
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+        self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
 
-        # Create player sprite
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
-                                           SPRITE_SCALING_PLAYER)
-        # Set player location
+        self.score = 0
+
+        # region Set up player
+        image_source = path("assets/images/skapning-export.png")
+        self.player = arcade.Sprite(image_source, 1, hit_box_algorithm='Simple')
+        self.player.newJump = True
+        self.player.combo_jump_timer = 0
+        self.player.combo_jumps = 0
+        self.player.on_ladder = False
+        self.player.on_ground = False
+
         grid_x = 3
         grid_y = 3
-        self.player_sprite.center_x = SPRITE_SIZE * grid_x + SPRITE_SIZE / 2
-        self.player_sprite.center_y = SPRITE_SIZE * grid_y + SPRITE_SIZE / 2
+        self.player.center_x = SPRITE_SIZE * grid_x + SPRITE_SIZE / 2
+        self.player.center_y = SPRITE_SIZE * grid_y + SPRITE_SIZE / 2
         # Add to player sprite list
-        self.player_list.append(self.player_sprite)
+        self.player_list.append(self.player)
 
         # --- Pymunk Physics Engine Setup ---
 
@@ -174,7 +195,7 @@ class GameWindow(arcade.Window):
         # Friction is between two objects in contact. It is important to remember
         # in top-down games that friction moving along the 'floor' is controlled
         # by damping.
-        self.physics_engine.add_sprite(self.player_sprite,
+        self.physics_engine.add_sprite(self.player,
                                        friction=PLAYER_FRICTION,
                                        mass=PLAYER_MASS,
                                        moment=arcade.PymunkPhysicsEngine.MOMENT_INF,
@@ -189,13 +210,13 @@ class GameWindow(arcade.Window):
         # PymunkPhysicsEngine.KINEMATIC objects will move, but are assumed to be
         # repositioned by code and don't respond to physics forces.
         # Dynamic is default.
-        self.physics_engine.add_sprite_list(self.ground_list,
+        self.physics_engine.add_sprite_list(self.scene["Ground"],
                                             friction=WALL_FRICTION,
                                             collision_type="wall",
                                             body_type=arcade.PymunkPhysicsEngine.STATIC)
 
         # Create the items
-        self.physics_engine.add_sprite_list(self.item_list,
+        self.physics_engine.add_sprite_list(self.scene["Item"],
                                             friction=DYNAMIC_ITEM_FRICTION,
                                             collision_type="item")
 
@@ -219,23 +240,46 @@ class GameWindow(arcade.Window):
 
     def on_update(self, delta_time):
         """ Movement and game logic """
-
+        self.player.on_ground = self.physics_engine.is_on_ground(self.player)
         # Update player forces based on keys pressed
         if self.left_pressed and not self.right_pressed:
             # Create a force to the left. Apply it.
             force = (-PLAYER_MOVE_FORCE_ON_GROUND, 0)
-            self.physics_engine.apply_force(self.player_sprite, force)
+            self.physics_engine.apply_force(self.player, force)
             # Set friction to zero for the player while moving
-            self.physics_engine.set_friction(self.player_sprite, 0)
+            self.physics_engine.set_friction(self.player, 0)
         elif self.right_pressed and not self.left_pressed:
             # Create a force to the right. Apply it.
             force = (PLAYER_MOVE_FORCE_ON_GROUND, 0)
-            self.physics_engine.apply_force(self.player_sprite, force)
+            self.physics_engine.apply_force(self.player, force)
             # Set friction to zero for the player while moving
-            self.physics_engine.set_friction(self.player_sprite, 0)
+            self.physics_engine.set_friction(self.player, 0)
         else:
             # Player's feet are not moving. Therefore, up the friction so we stop.
-            self.physics_engine.set_friction(self.player_sprite, 1.0)
+            self.physics_engine.set_friction(self.player, 1.0)
+
+        # Jumping mechanics
+        if self.player.on_ground:
+            print("On ground")
+
+        self.score_text = f"Score: {int(self.score)}, there are {len(self.scene['Coins'])} remaining"
+
+        # Keep track of time
+        self.real_timer_from_right = TIMER_FROM_RIGHT
+        self.total_time += delta_time
+        minutes = int(self.total_time) // 60
+        seconds = int(self.total_time) % 60
+        self.milliseconds = int((self.total_time - seconds) * 100 // 1)
+        self.clock_text = f"{minutes}:{seconds}"
+        x = 1
+        self.real_timer_from_right += 7
+        while x < len(self.clock_text):
+            self.real_timer_from_right += 14
+            x += 1
+
+        # Move the camera
+        self.center_camera_on_player()
+
 
         # Move items in the physics engine
         self.physics_engine.step()
@@ -243,10 +287,58 @@ class GameWindow(arcade.Window):
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
-        self.ground_list.draw()
-        self.bullet_list.draw()
-        self.item_list.draw()
-        self.player_list.draw()
+
+        # Make the camera follow the player
+        self.player_camera.use()
+
+        # Draw the game
+        self.scene.draw()
+        #self.scene.draw_hit_boxes()
+
+        # Draw the gui
+        self.gui_camera.use()
+        # Draw the score counter
+        arcade.draw_text(
+            self.score_text,
+            SCORE_FROM_LEFT,
+            SCREEN_HEIGHT - SCORE_FROM_TOP,
+            WHITE,
+            18
+        )
+
+        # Draw the timer
+        arcade.draw_text(
+            self.clock_text,
+            SCREEN_WIDTH - self.real_timer_from_right,
+            SCREEN_HEIGHT - TIMER_FROM_TOP,
+            WHITE,
+            18
+        )
+
+        arcade.draw_text(
+            f":{self.milliseconds}",
+            SCREEN_WIDTH - TIMER_FROM_RIGHT,
+            SCREEN_HEIGHT - TIMER_FROM_TOP,
+            WHITE,
+            18
+        )
+
+    def center_camera_on_player(self):
+        screen_center_x = self.player.center_x - (self.player_camera.viewport_width / 2)
+        screen_center_y = self.player.center_y - (self.player_camera.viewport_height / 2)
+
+        # Stop it from scrolling past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+
+        player_centered = screen_center_x, screen_center_y
+
+        self.player_camera.move_to(player_centered)
+
+
+
 
 
 def main():
